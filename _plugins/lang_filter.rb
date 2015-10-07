590bcc5a6c46c33e9e6d4498bfe3e83c9e8c54a5
Stephan Renatus <srenatus@posteo.de>
2015-10-07 13:20:32 +0000

    Fix lang-filter
    Forgot that String responds to #[], too

--- _plugins/lang_filter.rb
+++ _plugins/lang_filter.rb
@@ -3,7 +3,11 @@ module Jekyll
   module LangFilter
     def i18n(thing)
       lang = @context.registers[:page]['language']
-      thing[lang] || thing
+      if thing.is_a?(Hash) && thing.has_key?(lang)
+        thing[lang]
+      else
+        thing
+      end
     end
   end
 end
