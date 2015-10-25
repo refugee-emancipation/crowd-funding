839291c554b8ec31124e52e0ff35cac60e2a7088
Stephan Renatus <srenatus@posteo.de>
2015-10-25 10:39:06 +0100

    Start with third language: fr
    Changed the `lang_filter.rb` plugin to warn if the requested language isn't found, e.g.
    
    ```
    WARN: using default language (de) 'E-Mail Adresse' instead of page language (fr) in page 'index.md'
    ```
    
    TODO:
    - `.htaccess`
    
    Translation TODO:
    - `_data/navigation.yaml`
    - `_data/language.yaml`
    - `_include/betterplace-box.fr.html`
    - `pages/*.fr.md`

--- _plugins/lang_filter.rb
+++ _plugins/lang_filter.rb
@@ -3,8 +3,12 @@ module Jekyll
   module LangFilter
     def i18n(thing)
       lang = @context.registers[:page]['language']
+      default_lang = @context.registers[:site].config['language_default']
       if thing.is_a?(Hash) && thing.has_key?(lang)
         thing[lang]
+      elsif thing.is_a?(Hash)
+	puts "WARN: using default language (#{default_lang}) '#{thing[default_lang]}' instead of page language (#{lang}) in page '#{@context.registers[:page]['name']}'"
+        thing[default_lang]
       else
         thing
       end
